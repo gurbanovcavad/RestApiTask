d3f614c67f8ed7bd34e1c15b253204d4f8c38036
gurbanovcavad <gurbanovcavad7@gmail.com>
2025-12-27 17:00:34 +0400

    Update Env Example

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
-    DEBUG=(bool, False),
+    DJANGO_DEBUG=(bool, False),
     )
 
 environ.Env.read_env(BASE_DIR.parent / ".env")
